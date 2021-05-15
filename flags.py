66eb4ebe79fe7e87c83ae98fae7f076cee548423
zigaPerne <ziga.perne@gmail.com>
2021-05-15 11:06:47 +0200

    typos

--- flags.py
+++ flags.py
@@ -7,6 +7,6 @@ def parse():
 	parser.add_argument("-wms", "--wait-miliseconds", dest = "wait_miliseconds",  help = "Specify time between downloading images in ms", type = float)
 	parser.add_argument("-i", "--include", dest = "include", help = "Download ONLY specified filetype. Include multiple filetypes separated with commas in quotes. (ex: -i \"jpg, gif\")")
 	parser.add_argument("-e", "--exclude", dest = "exclude", help = "Download all filetypes except specified. Include multiple filetypes separated with commas in quotes. (ex: -e \"jpg, gif\")")
-	parser.add_argument("-d", "--duplicate", dest = "duplicate", default = 1, help = "Check if image filename already exists in the directory. If yes, the image will not be downloaded. If no, file will be replaced. Values: 1 = check; 0 = don't check; Default = 1", type = int)
+	parser.add_argument("-d", "--duplicate", dest = "duplicate", default = 1, help = "Check if image filename already exists in the directory and skip download. Values: 1 = check; 0 = don't check (replaces file); Default = 1", type = int)
 
 	return(parser.parse_args())
